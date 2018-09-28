8371d73c6de201b8ff8b1b12a3c1c48bf5d3e391
s-nt-s <santos82h@gmail.com>
2018-09-28 16:59:45 +0200

    ver contenido html

--- web/graby.php
+++ web/graby.php
@@ -7,8 +7,22 @@ if (array_key_exists("url", $_GET)) {
 
     $graby = new Graby();
     $result = $graby->fetchContent($article);
-
+if (array_key_exists("html", $_GET) && array_key_exists("html", $result)) {
+    header('Content-type: text/html; charset=utf-8');
+    ?>
+<!DOCTYPE html>
+<html lang="<?= $result['language'] ?>">
+<head>
+    <title><?= $result['title'] ?></title>
+</head>
+<body>
+<?= $result['html'] ?>
+</body>
+</html>
+    <?php
+} else {
     header('Content-type: application/json');
     echo json_encode($result);
+}
 }
 ?>
